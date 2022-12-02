8d4e79be9ac79a9cf305882af577b9dd2a20adee
LeoSallet <112955678+JuanHarki@users.noreply.github.com>
2022-12-02 17:42:13 +0100

    Update GuessWord.go

--- GuessWord.go
+++ GuessWord.go
@@ -1,15 +1,11 @@
 package hangman
 
 func GuessWord(word string, underscore []string, input string, lettre_list []string) []string {
-	stop := []string{"S", "T", "O", "P"}
+
 	bad_word := []string{"B", "A", "D", "!"}
 	same_word := []string{"S", "A", "M", "E", "!"}
-	list := []string{"/", "L", "I", "S", "T"}
-	if input == "STOP" {
-		return stop
-	} else if input == "/LIST" {
-		return list
-	} else if len(input) == 1 {
+
+	if len(input) == 1 {
 		for _, l := range lettre_list {
 			if l == input {
 				return same_word
